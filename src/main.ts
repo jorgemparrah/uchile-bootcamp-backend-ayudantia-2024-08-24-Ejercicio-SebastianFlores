5e13b111540b704daec3be4c003c27610eeada92
seba <seba.flores.ram@gmail.com>
2024-08-20 18:50:19 -0400

    Configuración inicial

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('API banquify')
+    .setDescription('Esta api describe la plataforma banquify')
+    .setVersion('1.0.0')
+    .addTag('banquify-api')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document, {
+    yamlDocumentUrl: 'swagger/yaml',
+  });
+
   await app.listen(3000);
 }
 bootstrap();
